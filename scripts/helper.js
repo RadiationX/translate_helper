f8667b93dbb9daa0d17e3163f8afc6e848836914
radiationx <radiationx@yandex.ru>
2016-06-28 13:57:18 +0300

    fix variables, fix special char

--- scripts/helper.js
+++ scripts/helper.js
@@ -13,7 +13,7 @@ function find() {
         names.push(matches[2]);
         values.push(matches[3]);
     }
-    
+
     //Сортировка по name, с "привязкой" к остальным полям
     length = names.length;
     for (var i = 0; i < length; i++) {
@@ -33,9 +33,13 @@ function find() {
             }
         }
     }
+    //Строка с value для перевода
+    var toTranslate = values.join('.\n') + '.';
+    //Экранирование спецю.символов и переменных
+    toTranslate = toTranslate.replace(/(\\\w)/gi, '|||$1|||').replace(/(%\w)/gi, '|||$1|||');
     //Записываем значения в поле для перевода
-    document.getElementById("out_to_translate").value = values.join('.\n') + '.';
-    
+    document.getElementById("out_to_translate").value = toTranslate;
+
     //Вывод сортировки
     var result = '<?xml version="1.0" encoding="utf-8"?>\n<resources>\n';
     for (var i = 0; i < strings.length; i++)
@@ -46,12 +50,18 @@ function find() {
 
 function replaceValues() {
     //Берем значения из поля с переведенными значениями
-    values = document.getElementById("in_to_translate").value.split(".\n");
-    
+    var toTranslate = document.getElementById("in_to_translate").value;
+    //"Вскрываем" экранирование
+    toTranslate = toTranslate.replace(
+        /\|\|\|(\\\w)\|\|\|/gi, '$1').replace(/\|\|\|(%\w)\|\|\|/gi, '$1')
+    var newValues = toTranslate.split(".\n");
+
     //Вывод перевода
     var result = '<?xml version="1.0" encoding="utf-8"?>\n<resources>\n';
+    //Тернарный оператор используется для исключения полей с переведенными переменными
     for (var i = 0; i < strings.length; i++)
-        result += '\t' + strings[i] + values[i] + '<\/string\n';
+        result += '\t' + strings[i] + (
+            /@[^\/]*\//i.test(newValues[i]) ? values[i] : newValues[i]) + '<\/string\n';
     result += '<\/resources>';
     document.getElementById("output_translated").value = result;
 }
